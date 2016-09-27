2cf12fb3295f10b1a7ffddffbf9f91600ea4d497
swapnil.kamble02@gmail.com <swapnil.kamble02@gmail.com>
2016-09-27 10:33:12 +0530

    Demo App Only

--- MockWInApp/Form1.Designer.cs
+++ MockWInApp/Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SearchPanel = new System.Windows.Forms.Panel();
+            this.btnDeselect = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.lstOutputBox = new System.Windows.Forms.ListBox();
+            this.lstInputBox1 = new System.Windows.Forms.ListBox();
+            this.SearchPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SearchPanel
+            // 
+            this.SearchPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SearchPanel.Controls.Add(this.btnDeselect);
+            this.SearchPanel.Controls.Add(this.btnSelect);
+            this.SearchPanel.Controls.Add(this.lstOutputBox);
+            this.SearchPanel.Controls.Add(this.lstInputBox1);
+            this.SearchPanel.Location = new System.Drawing.Point(12, 12);
+            this.SearchPanel.Name = "SearchPanel";
+            this.SearchPanel.Size = new System.Drawing.Size(554, 255);
+            this.SearchPanel.TabIndex = 0;
+            // 
+            // btnDeselect
+            // 
+            this.btnDeselect.Location = new System.Drawing.Point(250, 115);
+            this.btnDeselect.Name = "btnDeselect";
+            this.btnDeselect.Size = new System.Drawing.Size(46, 23);
+            this.btnDeselect.TabIndex = 3;
+            this.btnDeselect.Text = "<";
+            this.btnDeselect.UseVisualStyleBackColor = true;
+            this.btnDeselect.Click += new System.EventHandler(this.btnDeselect_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(250, 58);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(45, 23);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = ">";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // lstOutputBox
+            // 
+            this.lstOutputBox.FormattingEnabled = true;
+            this.lstOutputBox.Location = new System.Drawing.Point(408, 15);
+            this.lstOutputBox.Name = "lstOutputBox";
+            this.lstOutputBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstOutputBox.Size = new System.Drawing.Size(120, 212);
+            this.lstOutputBox.TabIndex = 1;
+            this.lstOutputBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lstOutputBox_KeyDown);
+            // 
+            // lstInputBox1
+            // 
+            this.lstInputBox1.FormattingEnabled = true;
+            this.lstInputBox1.Location = new System.Drawing.Point(18, 15);
+            this.lstInputBox1.Name = "lstInputBox1";
+            this.lstInputBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstInputBox1.Size = new System.Drawing.Size(120, 212);
+            this.lstInputBox1.TabIndex = 0;
+            this.lstInputBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lstInputBox_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 282);
+            this.Controls.Add(this.SearchPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.SearchPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel SearchPanel;
+        private System.Windows.Forms.ListBox lstOutputBox;
+        private System.Windows.Forms.ListBox lstInputBox1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnDeselect;
     }
 }
 
--- MockWInApp/Form1.cs
+++ MockWInApp/Form1.cs
@@ -15,6 +15,122 @@ namespace MockWInApp
         public Form1()
         {
             InitializeComponent();
+            FillList1();
+
+
+        }
+
+
+
+    internal void FillList1()
+        {
+            var lst = new List<string>();
+            lst.Add("A");
+            lst.Add("B");
+            lst.Add("C");
+            lst.Add("D");
+            lst.Add("F");
+            lstInputBox1.DataSource = lst;
+
+
+
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            var lstOPTemp = new List<string>();
+            var lstInputTemp = new List<String>();
+            if (lstInputBox1.SelectedItems.Count==0)
+            {
+                MessageBox.Show("Please Select");
+                return;
+            }
+            foreach (var item in lstInputBox1.Items)
+            {
+                lstInputTemp.Add(item.ToString());
+            }
+            if (lstOutputBox.Items.Count>0)
+            {
+                foreach (var item in lstOutputBox.Items)
+                {
+                    lstOPTemp.Add(item.ToString());
+                }
+            }
+            foreach (var item in lstInputBox1.SelectedItems)
+            {
+                lstOPTemp.Add(item.ToString());
+                lstInputTemp.Remove(item.ToString());
+            }
+
+            lstInputBox1.DataSource = lstInputTemp;
+            lstOutputBox.DataSource = lstOPTemp;
+
+
+
+
+
+        }
+
+        private void btnDeselect_Click(object sender, EventArgs e)
+        {
+            var lstOPTemp = new List<string>();
+            var lstInputTemp = new List<String>();
+            if (lstOutputBox.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Please Select");
+                return;
+            }
+            foreach (var item in lstOutputBox.Items)
+            {
+                lstOPTemp.Add(item.ToString());
+            }
+            if (lstInputBox1.Items.Count > 0)
+            {
+                foreach (var item in lstInputBox1.Items)
+                {
+                    lstInputTemp.Add(item.ToString());
+                }
+            }
+            foreach (var item in lstOutputBox.SelectedItems)
+            {
+                lstInputTemp.Add(item.ToString());
+                lstOPTemp.Remove(item.ToString());
+            }
+
+            lstInputBox1.DataSource = lstInputTemp;
+            lstOutputBox.DataSource = lstOPTemp;
+
+
+
+
+        }
+
+        private void lstInputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && e.Control)
+            {
+                
+                for (int i = 0; i < lstInputBox1.Items.Count; i++)
+                {
+                    lstInputBox1.SetSelected(i, true);
+                }
+
+
+            }
+        }
+
+        private void lstOutputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A && e.Control)
+            {
+
+
+                for (int i = 0; i < lstOutputBox.Items.Count; i++)
+                {
+                    lstOutputBox.SetSelected(i, true);
+                }
+
+            }
         }
     }
 }
